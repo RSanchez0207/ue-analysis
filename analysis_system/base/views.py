4656100c5620821e0c7f7a9ba482bd52ae5487eb
RSanchez0207 <sanchez.raighen@ue.edu.ph>
2022-04-18 21:52:28 +0800

    Added backedn for dynamic image source

--- analysis_system/base/views.py
+++ analysis_system/base/views.py
@@ -8,23 +8,41 @@ from datetime import timedelta,date
 from .models import SentimentOutput, EmotionOutput, SurveyResponse
 from .decorators import allowed_users, unauthenticated_user
 
+today = date.today()
+yesterday = today - timedelta(days = 1)
+
+format = yesterday.strftime('%b-%d-%y')
+static_dir_str_charts = "../../static/base/charts/"
+filename_SA =  static_dir_str_charts + "SA" + str(yesterday) + ".png"
+filename_ER =  static_dir_str_charts + "ERFIG" + str(yesterday) + ".png"
+filename_ER_TOP = static_dir_str_charts + "ERFIGTOP" + str(yesterday) + ".png"
+
+filename_Q1 = static_dir_str_charts + "LS" + str(yesterday) + "comm.png"
+filename_Q2 = static_dir_str_charts + "LS" + str(yesterday) + "prof.png"
+filename_Q3 = static_dir_str_charts + "LS" + str(yesterday) + "conn.png"
+filename_Q4 = static_dir_str_charts + "LS" + str(yesterday) + "avail.png"
+filename_Q5 = static_dir_str_charts + "LS" + str(yesterday) + "dpa.png"
+
 # Dashboard View
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin','college','student'])
 def dashboard(request):
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
-    format = yesterday.strftime('%b-%d-%y')
-    return render(request, 'dashboard.html', {'format': format})
+
+    return render(request, 'dashboard.html', {'format': format, 
+                                            'filename_SA': filename_SA,
+                                            'filename_ER': filename_ER,
+                                            'filename_ER_TOP': filename_ER_TOP,
+                                            'filename_Q1': filename_Q1,
+                                            'filename_Q2': filename_Q2,
+                                            'filename_Q3': filename_Q3,
+                                            'filename_Q4': filename_Q4,
+                                            'filename_Q5': filename_Q5,})
 
 # Survey History View
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin','college'])
 def history(request):
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
     survey_list = SurveyResponse.objects.all()
-    format = yesterday.strftime('%b-%d-%y')
     return render(request, 'survey-history.html', {'survey_list': survey_list, 'format': format})
 
 # Sentiment View
@@ -32,9 +50,6 @@ def history(request):
 @allowed_users(allowed_roles=['admin','college','student'])
 def sentiment(request):
     sentiment_list = SentimentOutput.objects.all()
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
-    format = yesterday.strftime('%b-%d-%y')
     return render(request, 'sentiment-analysis.html',{'sentiment_list': sentiment_list, 'format': format })
 
 # Emotion View
@@ -42,21 +57,14 @@ def sentiment(request):
 @allowed_users(allowed_roles=['admin','college','student'])
 def emotion(request):
     emotion_list = EmotionOutput.objects.all()
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
-    format = yesterday.strftime('%b-%d-%y')
     return render(request, 'emotion-recognition.html', {'emotion_list': emotion_list, 'format': format})
 
 # Privacy Policy View
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin','college','student'])
 def privacy(request):
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
-    format = yesterday.strftime('%b-%d-%y')
     return render(request, 'privacy-policy.html', {'format': format})
 
-@unauthenticated_user
 # Login Page View
 def loginPage(request):
     if request.method == 'POST':
